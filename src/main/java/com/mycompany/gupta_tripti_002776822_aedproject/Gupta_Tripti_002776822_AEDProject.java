42a98cce8ffb99a3ddeea264259f5cc684a207cf
gtriptineu <g.tripti@northeastern.edu>
2022-09-27 01:35:01 -0400

    test1

---
+++ src/main/java/com/mycompany/gupta_tripti_002776822_aedproject/Gupta_Tripti_002776822_AEDProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gupta_tripti_002776822_aedproject;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Gupta_Tripti_002776822_AEDProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
