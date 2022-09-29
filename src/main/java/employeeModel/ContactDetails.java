17d6a3893de76e94ac30cfda4999a15fab1e1288
gtriptineu <g.tripti@northeastern.edu>
2022-09-29 16:12:55 -0400

    Employee Class

---
+++ src/main/java/employeeModel/ContactDetails.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employeeModel;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ContactDetails {
+    private String phoneNumber;
+    private String emailAddress;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    
+    
+}
---
+++ src/main/java/employeeModel/EmployeeDetails.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employeeModel;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class EmployeeDetails {
+    private String employeeName;
+    private int employeeId;
+    private int employeeAge;
+    private String employeeGender;
+    private Date employeeStartDate;
+    private String employeeLevel;
+    private String teamInfo;
+    private String positionTitle;
+    private ContactDetails contactDetails;
+    private String photoUrl;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getEmployeeAge() {
+        return employeeAge;
+    }
+
+    public void setEmployeeAge(int employeeAge) {
+        this.employeeAge = employeeAge;
+    }
+
+    public String getEmployeeGender() {
+        return employeeGender;
+    }
+
+    public void setEmployeeGender(String employeeGender) {
+        this.employeeGender = employeeGender;
+    }
+
+    public String getEmployeeLevel() {
+        return employeeLevel;
+    }
+
+    public void setEmployeeLevel(String employeeLevel) {
+        this.employeeLevel = employeeLevel;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public ContactDetails getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(ContactDetails contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public Date getEmployeeStartDate() {
+        return employeeStartDate;
+    }
+
+    public void setEmployeeStartDate(Date employeeStartDate) {
+        this.employeeStartDate = employeeStartDate;
+    }
+    
+    
+}
